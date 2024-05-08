58e3ad5d6eafda21c0f2ccd421f82690217d9b57
IzzyK <G00424523@ATU.IE>
2024-05-08 01:46:54 +0100

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,42 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { FormsModule } from '@angular/forms';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, 
+  IonItem, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle,
+IonCardContent } from '@ionic/angular/standalone';
 import { RouterLinkWithHref } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { WeatherInfoService } from '../Services/weather-info.service';
 
 const API_URL = environment.API_URL;
 const API_KEY = environment.API_KEY;
-
-interface WeatherResponse {
-  main: {
-    // Define the properties you expect under 'main'
-    temp: number; // Assuming 'temp' is a property of the 'main' object and it's a number
-    // You can include other properties as needed
-  };
-  // Define other properties if they exist in the response
-}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, CommonModule, FormsModule, RouterLinkWithHref, HttpClientModule, IonItem],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, CommonModule, FormsModule, 
+  RouterLinkWithHref, HttpClientModule, IonItem, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle,
+  IonCardContent],
 })
 export class HomePage {
-  temp :number = 0;
+  weatherInfo:any[]= [];
 
   constructor(public httpClient: HttpClient) {
-    this.loadData();
-  }
 
-  loadData() {
-    this.httpClient.get<WeatherResponse>(`${API_URL}/weather?lat=${51.50853000}&lon=${-0.12574000}&appid=${API_KEY}`).subscribe((results : WeatherResponse) => {
-      console.log(results);
-      this.temp = results.main.temp;
-      console.log(this.temp);
-    });
   }
 }
--- src/app/weather/weather.page.ts
+++ src/app/weather/weather.page.ts
@@ -1,20 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'; 
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, 
+  IonItem, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle,
+IonCardContent } from '@ionic/angular/standalone';
+import { RouterLinkWithHref } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { WeatherInfoService } from '../Services/weather-info.service';
+
+const API_URL = environment.API_URL;
+const API_KEY = environment.API_KEY;
+
+
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.page.html',
   styleUrls: ['./weather.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, CommonModule, FormsModule, 
+    RouterLinkWithHref, HttpClientModule, IonItem, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle,
+    IonCardContent]
 })
 export class WeatherPage implements OnInit {
 
-  constructor() { }
+  weatherInfo:any[]= [];
 
-  ngOnInit() {
+  constructor(public httpClient: HttpClient) {
+    this.loadData();
   }
 
+  ngOnInit(): void {
+    
+  }
+
+  loadData() {
+    this.httpClient.get<any>(`${API_URL}/weather?lat=${51.50853000}&lon=${-0.12574000}&appid=${API_KEY}`).subscribe((result) => {
+      this.weatherInfo.push(result);
+      console.log(result);
+    });
+  }
 }
